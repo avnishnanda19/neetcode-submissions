7f350ac913d8a5c732a328d6bcacaa06882d8ab3
avnishnanda19 <42563725+avnishnanda19@users.noreply.github.com>
2026-05-26 12:34:52 +0530

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.java
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        Collections.sort(intervals, (a,b)->Integer.compare(a.start, b.start));
+
+        int rooms =0;
+        PriorityQueue<Integer> q= new PriorityQueue<>(); 
+        for(Interval interval : intervals)
+        {
+            while(!q.isEmpty() && q.peek()<=interval.start)
+            {
+                q.poll(); 
+            }
+            q.offer(interval.end);
+            rooms= Math.max(rooms, q.size());
+        }
+        return rooms;
+
+    }
+}
