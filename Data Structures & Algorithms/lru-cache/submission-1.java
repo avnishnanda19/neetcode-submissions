7c8ca313325c5205e91389d5b36049fab696e3ce
avnishnanda19 <42563725+avnishnanda19@users.noreply.github.com>
2026-05-28 10:12:32 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,95 @@
+class LRUCache {
+    class Node{
+        int key; 
+        int value;
+        Node next;
+        Node prev; 
+        public Node(int key , int value)
+        {
+            this.key=key; 
+            this.value=value;
+            this.next=null; 
+            this.prev=null;
+        }
+        public Node()
+        {
+            this(-1,-1);
+        }
+
+    }
+
+    Map<Integer, Integer> cache;
+    Map<Integer, Node> nodeMap;
+    int capacity; 
+    Node head ;
+    Node tail ;
+    public LRUCache(int capacity) {
+        this.cache = new HashMap<>(); 
+        this.nodeMap = new HashMap<>(); 
+        this.capacity = capacity; 
+        this.head = new Node();
+        this.tail= head;
+    }
+    
+    public int get(int key) {
+        if(!cache.containsKey(key))
+            return -1;
+        
+        Node n = nodeMap.get(key);
+        removeNodeFromList(n);
+
+        insertToHead(n);
+        return n.value;
+    }
+    
+    public void put(int key, int value) {
+        Node n = null;
+        if(!cache.containsKey(key))
+        {
+            n = new Node(key,value);
+            nodeMap.put(key, n);
+        }
+        else {
+            n = nodeMap.get(key); 
+            n.value= value;
+            removeNodeFromList(n);
+        }
+        insertToHead(n);
+        cache.put(key,value);
+        // delete LRU
+        if(cache.size()>capacity)
+        {
+            deleteTail();
+        }
+    }
+    
+    private void removeNodeFromList(Node n)
+    {
+        n.prev.next =  n.next;
+        if(n.next!=null) n.next.prev =n.prev;
+        if(tail==n) tail=n.prev;
+    }
+    private void insertToHead(Node n)
+    {
+        n.next=head.next;
+        if(head.next!=null)
+        head.next.prev = n;
+        head.next=  n; 
+        n.prev = head;
+
+        if(head==tail)
+        {
+            tail=n;
+        }
+    }
+    private void deleteTail()
+    {
+        Node last = tail; 
+        cache.remove(last.key);
+        nodeMap.remove(last.key); 
+
+        last.prev.next= null;
+        tail=last.prev;
+    }
+
+}
