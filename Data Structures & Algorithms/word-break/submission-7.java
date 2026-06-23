847adaabf5388d58f96d3d93189fe968b403d3fb
avnishnanda19 <42563725+avnishnanda19@users.noreply.github.com>
2026-06-23 10:54:59 +0530

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp= new boolean[s.length()+1];
+        dp[s.length()]=true; 
+
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            for(String w : wordDict)
+            {
+                if(w.length()+i>s.length()) continue;
+                if(s.substring(i,i+w.length()).equals(w))
+                {
+                    dp[i]=dp[i+w.length()];
+                }
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+}
